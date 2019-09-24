a7ed541261df129b8f59bd78dee27edb990aae28
Luke <lukedavia@icloud.com>
2019-09-24 20:27:36 +1200

    add last logged in relationship

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,6 +8,7 @@ use Bluewing\BluewingMustVerifyEmail;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
@@ -51,7 +52,7 @@ class User extends Model implements MustVerifyEmailContract, CanResetPasswordCon
      * A `User`, either a student or an instructor, can be part of many `Organization`'s that use Horizon.
      * This is supported via a many to many relationship between the two entities.
      *
-     * @laravel-relation `User` belongsToMany `Organization`'s.
+     * @laravel-relation `User` belongsToMany `Organization`.
      *
      * @return BelongsToMany
      */
@@ -63,11 +64,23 @@ class User extends Model implements MustVerifyEmailContract, CanResetPasswordCon
      * The `UserOrganization` is the pivot between a `User` and an `Organization`, and represents a `Users`'s
      * link with an `Organization`. As such, a `User` can have many `UserOrganization`'s.
      *
-     * @laravel-relation `User` hasMany `UserOrganization`'s.
+     * @laravel-relation `User` hasMany `UserOrganization`.
      *
      * @return HasMany
      */
     public function userOrganizations() {
         return $this->hasMany('Bluewing\Models\UserOrganization', 'userId');
+    }
+
+    /**
+     * A `User` object stores the last `Organization` they were logged into. This is so that when they log out and log in
+     * again, the appropriate `Organization` context can be set.
+     *
+     * @laravel-relation `User` hasOne `Organization`.
+     *
+     * @return HasOne
+     */
+    public function lastLoggedInOrganization() {
+        return $this->hasOne('Bluewing\Models\Organization', 'lastLoggedInOrganizationId');
     }
 }
