43015817036c9e831808a09738c5cb78cac5f1dd
Luke <lukedavia@icloud.com>
2019-12-24 13:19:23 +1300

    adjustments to bluewinguserprovider to ensure tenancy is preserved in dev

--- src/app/Providers/BluewingUserProvider.php
+++ src/app/Providers/BluewingUserProvider.php
@@ -2,10 +2,12 @@
 
 namespace Bluewing\Providers;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\EloquentUserProvider;
 use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
  * The `BluewingUserProvider` provides an extension of the default Laravel `EloquentUserProvider` implementation
@@ -61,6 +63,15 @@ class BluewingUserProvider extends EloquentUserProvider implements UserProvider
     /**
      * Retrieve a user by their unique identifier.
      *
+     * Note that unlike the `UserProvider` class, this method will never return `null`. If a `UserOrganization` ID is
+     * provided that does correspond with any `UserOrganization`, null would normally be returned. This would allow
+     * the JwtGuard class to not apply a tenancy scope to the appropriate queries, allowing tenancy-wide access.
+     *
+     * Fortunately, it would only be possible to provide a `UserOrganization` ID that does not exist by altering a
+     * client JSON Web Token with knowledge of the application key. As JWT's are immutable, this is therefore not a
+     * security concern. However, during development where such an event is possible, it is helpful to return 401
+     * Unauthorized as a reminder that the `UserOrganization` does not exist.
+     *
      * @param mixed $identifier - The identifier used to identify an `Authenticatable`.
      *
      * @return \Illuminate\Contracts\Auth\Authenticatable|null
@@ -69,10 +80,14 @@ class BluewingUserProvider extends EloquentUserProvider implements UserProvider
     {
         $model = $this->createModel();
 
-        return $this->newModelQuery($model)
-            ->withoutGlobalScopes()
-            ->where($model->getAuthIdentifierName(), $identifier)
-            ->first();
+        try {
+            return $this->newModelQuery($model)
+                ->withoutGlobalScopes()
+                ->where($model->getAuthIdentifierName(), $identifier)
+                ->firstOrFail();
+        } catch (ModelNotFoundException $e) {
+            abort(401);
+        }
     }
 
     /**
