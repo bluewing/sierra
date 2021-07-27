f7075a5b49308470f811d426ded3b851869e205d
Luke <lukedavia@icloud.com>
2021-07-28 09:39:13 +1200

    refactor: update regex for property doc comments on models

--- src/app/Schema/ManagesModelComments.php
+++ src/app/Schema/ManagesModelComments.php
@@ -29,13 +29,19 @@ trait ManagesModelComments
         $parsedPropertyComments = $this->parsePropertyDocTagsFromModel($reflection);
 
         foreach ($this->getColumns() as $columnDefinition) {
-            $columnDefinition->comment($parsedPropertyComments[$columnDefinition->name]);
+            if (array_key_exists($columnDefinition->name, $parsedPropertyComments)) {
+                $columnDefinition->comment($parsedPropertyComments[$columnDefinition->name]);
+            }
         }
     }
 
     /**
      * Retrieves the DocComment, parses it with regex, and remaps the result into an array of key values,
      * representing the name of the column and the description that should become the comment.
+     *
+     * @see https://regex101.com/r/me6weR/1 For a working example of the regex employed in this method.
+     * @see https://www.php.net/manual/en/language.variables.basics.php For documentation on extracting a valid
+     * variable name from a documentation comment.
      *
      * @param ReflectionClass $reflectionClass - The class to extract a PhpDocComment from, and parse out
      * `@property` tags.
@@ -54,7 +60,7 @@ trait ManagesModelComments
         }
 
         preg_match_all(
-            '/@property\s(?:[A-Za-z]*?)\s\$?([[:alnum:]]*?)\s-\s((?:(?!@property).)*)/s',
+            '/@property\s(?:[\S]*?)\s\$?([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*?)\s-\s((?:(?!@property).)*)/s',
             $modelComment,
             $matches,
             PREG_SET_ORDER
