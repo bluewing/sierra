8d10a00a93652c9cb1fb641729b8a1525e7c27d1
Luke <lukedavia@icloud.com>
2022-03-25 22:27:12 +1300

    fix: remove regular expression to validate coordinate component

--- src/app/Rules/IsCoordinateComponent.php
+++ src/app/Rules/IsCoordinateComponent.php
@@ -6,16 +6,19 @@ namespace Bluewing\Rules;
 abstract class IsCoordinateComponent
 {
     /**
-     * @param $value - The value to be checked for whether it's a coordinate component.
-     * @param $rangeInDegrees - The acceptable range in degrees that's allowed for the value.
+     * Validates that the coordinate component is of the correct type (either an int/float) and is in the correct range
+     * for the allowed coordinate type (i.e. -180 to 180 for longitudes, -90 to 90 for latitudes).
+     *
+     * @param mixed $value - The value to be checked for whether it's a coordinate component. This is declared to be of
+     * type `mixed`, but is validated as `int`/`float` inside the method.
+     * @param int $rangeInDegrees - The acceptable range in degrees that's allowed for the value.
      *
      * @return bool - `true` if the value appears to be a coordinate component, `false` otherwise.
      */
-    public function isCoordinate($value, int $rangeInDegrees): bool
+    public function isCoordinate(mixed $value, int $rangeInDegrees): bool
     {
-        return is_numeric($value)
+        return (is_int($value) || is_float($value))
             && $value >= $rangeInDegrees * -1
-            && $value <= $rangeInDegrees
-            && preg_match('/^-?\d{1,' . strlen($rangeInDegrees) . '}\.\d{0,6}$/', (string) $value) === 1;
+            && $value <= $rangeInDegrees;
     }
 }
